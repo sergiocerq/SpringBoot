6e0673707497affcb14a7d7cf3020a7896afa937
Sergio Cerqueira <126781655+sergiocerq@users.noreply.github.com>
2024-02-28 20:12:00 -0300

    feat: subindo mini aplicação com Socket em java

---
+++ Socket/client/Cliente.java
@@ -0,0 +1,21 @@
+package client;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente { 
+	public static void main(String args[]) throws IOException {
+		//TODO Auto-generated method sub
+		
+		Socket cliente = new Socket("10.133.15.1", 80);
+			
+		PrintWriter escrever = new PrintWriter(cliente.getOutputStream(), true);
+		Scanner ler = new Scanner(cliente.getInputStream());
+			
+		escrever.println("TESTANDO");
+
+		System.out.println(ler.nextLine());
+		
+	}
+}
